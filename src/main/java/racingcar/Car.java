f1ac2443167730449df0282a82f7fc15bf5b733c
yoonjuho <dbswn990@naver.com>
2022-07-16 05:26:11 +0900

    feat: Car객체 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,11 +2,21 @@ package racingcar;
 
 public class Car {
     private final String name;
+    MoveCondition moving;
     private int position = 0;
 
     public Car(String name) {
         this.name = name;
+        moving = new CurrentMove();
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void move(){
+        position += moving.forword();
     }
 
-    // 추가 기능 구현
+    public int getPosition() {
+        return position;
+    }
 }
