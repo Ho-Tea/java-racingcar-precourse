4273663e53d794dc8e95c41319a18f494ec0c6d2
yoonjuho <dbswn990@naver.com>
2022-07-16 05:26:40 +0900

    feat: 메인구현
    프로그램의 전체적인 순서도가 표현된다

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+
+
+        UserInput input = new UserInput();
+        UserOutput output = new UserOutput();
+        List<Car> cars = new ArrayList<>();
+
+        for(String carname : input.getCarName())
+            cars.add(new Car(carname));
+
+        output.printResult();
+        for(int i = input.getTrialNum();i > 0;i--){
+            for(Car car:cars)
+                car.move();
+            output.getProcess(cars);
+        }
+
+        output.getWinner(cars);
     }
 }
